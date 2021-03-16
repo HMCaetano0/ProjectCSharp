c051bba424f23e13b2cb5f59dffe99ab5601b6fe
higorcaetano <higorcaetano2909@gmail.com>
2021-03-16 13:36:29 -0300

    Adicionando Primeiro Código

---
+++ First Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace First_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia"!);
+
+            Console.ReadLine();
+        }
+    }
+}
